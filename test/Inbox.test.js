099032901d70e9d9849aaa469fa3cdccd8ae3b45
vitoraa <vitor.lima@outlook.com>
2022-05-30 12:01:25 -0300

    console log all accounts

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,3 +3,15 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
+
+beforeEach(async () => {
+    //Get a list of all accounts
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        assert.ok(true);
+    });
+});
