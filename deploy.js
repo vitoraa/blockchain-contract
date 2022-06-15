fdade91cb15af3cb02cc60002bec2d337b1669a6
vitoraa <vitor.lima@outlook.com>
2022-06-15 15:43:16 -0300

    connect to Rinkeby network

--- deploy.js
+++ deploy.js
@@ -1,1 +1,8 @@
-// deploy code will go here
+const HDwalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDwalletProvider('region range hold discover relief that consider thunder priority explain receive cactus',
+    'https://rinkeby.infura.io/v3/7fae2aed999247a8935ebff226d3e03b'
+);
+const web3 = new Web3(provider);
