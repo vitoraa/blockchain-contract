15d55d4ee94be67bf95d3b31a3f815fdca41cc62
vitoraa <vitor.lima@outlook.com>
2022-06-15 16:29:06 -0300

    deploy the contract

--- deploy.js
+++ deploy.js
@@ -6,3 +6,20 @@ const provider = new HDwalletProvider('region range hold discover relief that co
     'https://rinkeby.infura.io/v3/7fae2aed999247a8935ebff226d3e03b'
 );
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode,
+            arguments: ['Hi there!']
+        })
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+    console.log(result.options.address);
+    provider.engine.stop();
+}
+deploy();
